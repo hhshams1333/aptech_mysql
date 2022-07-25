c916b2f76c63d0d975a084132cc7b9c13660f35e
hhshams1333 <hhshams1333@gmail.com>
2022-07-25 19:26:40 +0430

    mysql project for aptech - ostad lotfi

---
+++ 10_queries.sql
@@ -0,0 +1,33 @@
+ -- truncate table products;
+
+-- 1
+-- select * from products;
+
+-- 2
+-- select count(100) from products;
+
+-- 3
+-- select name , max(price) from products;
+
+-- 4 
+-- select sum(price) from products;
+
+-- 5 
+-- delete from products where name='product25585';
+
+-- 6
+-- SELECT * FROM products WHERE name = ‘product1’ OR price > 25;
+
+-- 7
+-- SELECT name FROM products WHERE price < 25; 
+
+-- 8 
+select name, entry_date from products;
+
+-- 9
+-- select count(name) from products where price >26;
+
+-- 10
+-- select avg(price) from products ;
+
+
